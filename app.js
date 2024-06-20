844c04fbc767c64630690ecb82f868fb14c029d7
kirnjiyun <kimjiyunee@naver.com>
2024-06-20 21:47:14 +0900

    project setting

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+const mongoURI = `mongodb://localhost:27017/aloneshoppingmall`;
+
+app.use(bodyParser.json());
+
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Mongoose connected");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+connectToDatabase();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
