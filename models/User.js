c4c2487383e657d7a9a62e189f098ef2cc2989cc
kirnjiyun <kimjiyunee@naver.com>
2024-06-20 23:12:37 +0900

    유저 모델 만들기 #1

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const userSchema = Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        role: { type: String, enum: ["user", "admin"], default: "user" },
+    },
+    { timestamps: true }
+);
+const User = mongoose.model("User", userSchema);
+module.exports = User;
