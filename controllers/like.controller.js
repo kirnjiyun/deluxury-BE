bd41d577b07cd708f590be484e92542a9865acc9
kirnjiyun <kimjiyunee@naver.com>
2024-07-01 19:21:55 +0900

    like #6

--- controllers/like.controller.js
+++ controllers/like.controller.js
@@ -12,6 +12,7 @@ likeController.addToLike = async (req, res) => {
                 .status(400)
                 .json({ status: "fail", error: "유효하지 않은 사용자입니다." });
         }
+
         if (!productId) {
             return res.status(400).json({
                 status: "fail",
@@ -23,52 +24,50 @@ likeController.addToLike = async (req, res) => {
             "items.productId"
         );
         if (!like) {
-            like = new like({ userId });
+            like = new Like({ userId });
             await like.save();
         }
 
-        console.log("like found or created:", like);
-
         const existItem = like.items.find(
-            (item) =>
-                item.productId._id.toString() === productId &&
-                item.size === size
+            (item) => item.productId._id.toString() === productId
         );
 
         if (existItem) {
             return res.status(400).json({
                 status: "fail",
-                error: "아이템이 이미 카트에 존재합니다.",
+                error: "아이템을 이미 찜하셨습니다.",
             });
         }
 
         const newItem = {
             productId: new mongoose.Types.ObjectId(productId),
-            size,
-            qty,
         };
+
         like.items.push(newItem);
         await like.save();
-        const populatedlike = await like
-            .findOne({ userId: userId })
-            .populate("items.productId");
-        const addedItem = populatedlike.items.find(
+
+        const populatedLike = await Like.findOne({ userId: userId }).populate(
+            "items.productId"
+        );
+
+        const addedItem = populatedLike.items.find(
             (item) => item.productId._id.toString() === productId
         );
+
         res.status(200).json({
             status: "success",
             item: addedItem,
         });
     } catch (error) {
-        console.error("Error occurred while adding item to like:", error);
+        console.error("아이템을 찜하는 동안 에러가 발생했습니다:", error);
         return res.status(400).json({ status: "fail", error: error.message });
     }
 };
 
-likeController.getlike = async (req, res) => {
+likeController.getLike = async (req, res) => {
     try {
         const { userId } = req;
-        const like = await like.findOne({ userId }).populate({
+        const like = await Like.findOne({ userId }).populate({
             path: "items",
             populate: {
                 path: "productId",
@@ -85,15 +84,24 @@ likeController.getlike = async (req, res) => {
     }
 };
 
-likeController.deletelikeItem = async (req, res) => {
+likeController.deleteLikeItem = async (req, res) => {
     try {
         const { id } = req.params;
         const { userId } = req;
-        const like = await like.findOne({ userId });
+        const like = await Like.findOne({ userId });
+        if (!like) {
+            return res
+                .status(400)
+                .json({ status: "fail", error: "찜 목록이 없습니다." });
+        }
+
         like.items = like.items.filter((item) => !item._id.equals(id));
 
         await like.save();
-        res.status(200).json({ status: 200, likeItemQty: like.items.length });
+        res.status(200).json({
+            status: "success",
+            likeItemQty: like.items.length,
+        });
     } catch (error) {
         return res.status(400).json({ status: "fail", error: error.message });
     }
--- models/Like.js
+++ models/Like.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const User = require("./User");
 const Product = require("./Product");
-const cartSchema = Schema(
+const likeSchema = Schema(
     {
         userId: { type: mongoose.ObjectId, ref: User },
         items: [
@@ -13,12 +13,12 @@ const cartSchema = Schema(
     },
     { timestamps: true }
 );
-cartSchema.methods.toJSON = function () {
+likeSchema.methods.toJSON = function () {
     const obj = this._doc;
     delete obj.__V;
     delete obj.updateAt;
     delete obj.createAt;
     return obj;
 };
-const Cart = mongoose.model("Like", likeSchema);
-module.exports = Cart;
+const Like = mongoose.model("Like", likeSchema);
+module.exports = Like;
